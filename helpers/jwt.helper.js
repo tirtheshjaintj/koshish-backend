f4d6e99e53bc28c2cd5b7002a265ad496efe1e9f
Tirthesh Jain <tirtheshjaintj@gmail.com>
2025-01-23 14:46:48 +0530

    Koshish Started

---
+++ helpers/jwt.helper.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.JWT_SECRET;
+const expiresIn = process.env.TOKEN_EXPIRATION;
+
+function setUser(user) {
+    return jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn });
+}
+function getUser(token) {
+    if (!token) return null;
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        return decoded;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+module.exports = { getUser, setUser };
