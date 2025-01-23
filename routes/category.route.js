b26b394ab3052c63f9626b526f7df8f8d4530713
Tirthesh Jain <tirtheshjaintj@gmail.com>
2025-01-23 15:05:41 +0530

    Backend Cleaned

--- routes/category.route.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const { getCategories, getCategory } = require('../controllers/category.controller');
-
-const router = Router();
-
-router.get("/", getCategories);
-router.get("/:category_id", getCategory);
-
-module.exports = router;
