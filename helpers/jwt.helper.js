dd3fc2db11bf995242c616187f496da9aa5d1d43
Tirthesh Jain <tirtheshjaintj@gmail.com>
2025-03-03 15:22:42 +0530

    Some Changes

--- helpers/jwt.helper.js
+++ helpers/jwt.helper.js
@@ -11,9 +11,11 @@ async function getUser(token) {
     if (!token) return null; 
     try {
         const decoded = jwt.verify(token, JWT_SECRET);
-        console.log(decoded)
-        const user = await User.findById(decoded.id);
-        
+        console.log(decoded);
+        let user = await User.findById(decoded.id);
+        if(!user){
+            user=await Class.findById(decoded.id);
+        }
         return user;
     } catch (err) {
         console.log(err);
